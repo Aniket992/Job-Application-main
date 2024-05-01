9bc1448ddbf23031955cca179f28c93d15760076
Aniket992 <78945155+Aniket992@users.noreply.github.com>
2024-05-01 11:29:38 +0530

    corrections

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -32,7 +32,7 @@ const Login = () => {
     }
 
     try {
-      const response = await fetch("https://job-application-main.vercel.app/api/v1/auth/login", {
+      const response = await fetch("https://server-git-main-aniket992s-projects.vercel.app//api/v1/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
