059f30651e7275f6c9f13221636c6725ea4c45ac
Aniket992 <78945155+Aniket992@users.noreply.github.com>
2024-05-01 10:50:10 +0530

    login changes

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -32,7 +32,7 @@ const Login = () => {
     }
 
     try {
-      const response = await fetch("http://localhost:8080/api/v1/auth/login", {
+      const response = await fetch("/api/v1/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
