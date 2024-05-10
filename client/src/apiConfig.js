41c2a7ae109ccc53bd41ce2548d543a2be63729b
Aniket992 <78945155+Aniket992@users.noreply.github.com>
2024-05-10 23:15:14 +0530

    base url update

--- client/src/apiConfig.js
+++ client/src/apiConfig.js
@@ -1,3 +1,3 @@
 // apiConfig.js
-// export const BASE_URL = 'https://job-application-main.onrender.com';
-export const BASE_URL = 'http://localhost:8080';
+export const BASE_URL = 'https://job-application-main.onrender.com';
+// export const BASE_URL = 'http://localhost:8080';
